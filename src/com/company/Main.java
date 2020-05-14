51fadffd4155308ed6d8987d6515e58fe8738510
User <paramonov7894561230@yandex.ru>
2020-05-14 21:50:32 +0300

    master - урок 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,50 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // 2) --
+        byte bit8 = 10;
+        short bit16 = 10;
+        int bit32 = 10;
+        long bit64 = 10L;
+
+        double b32 = 10.0;
+        float b64 = 10F;
+
+        boolean b = false;
+
+        char b16 = 10;
+        //----------
+
+    }
+
+    // 3)
+    public static int calculation(int a, int b, int c, int d) {
+        return a * (b + (c / d));
+    }
+
+    // 4)
+    private static boolean checkRangeSum(int a, int b) {
+        int sum = a + b;
+        return 10 <= sum && sum <= 20;
+    }
+
+    // 5)
+    private static void checkNum(int a) {
+        if (a >= 0) {
+            System.out.println(a + " : положительное число");
+        } else {
+            System.out.println(a + " : отрицательное число");
+        }
+    }
+
+    // 6)
+    private static boolean isMinusNum(int a) {
+        return a < 0;
+    }
+
+    // 7)
+    private static void printName(String name) {
+        System.out.println("Привет, " + name);
     }
 }

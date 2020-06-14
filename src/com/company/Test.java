cf989c4988658eeeb93d31d6a6f5518fcaf3529f
User <paramonov7894561230@yandex.ru>
2020-06-14 13:41:46 +0300

    задание__1 - готово

---
+++ src/com/company/Test.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Test {
+    public static void main(String[] args) {
+         Participant[] participants = {new Robot(), new Cat(), new Person(), new Cat()};
+         for (Participant p : participants) {
+             Treadmill.passObstacle(p);
+             Wall.passObstacle(p);
+             System.out.println();
+         }
+    }
+}
---
+++ src/com/company/Treadmill.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Treadmill {
+
+    public static void passObstacle(Participant action) {
+        action.run();
+    }
+}
